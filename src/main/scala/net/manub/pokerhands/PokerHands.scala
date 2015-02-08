10aa684e57c3c6e417ec19f4c2f44b2cc6d4357a
Emanuele Blanco <emanuele.blanco@gmail.com>
2015-02-08 21:54:28 +0100

    More progress on the score calculation. Generation of Game Outcome may need a refactoring.

--- src/main/scala/net/manub/pokerhands/PokerHands.scala
+++ src/main/scala/net/manub/pokerhands/PokerHands.scala
@@ -1,5 +1,7 @@
 package net.manub.pokerhands
 
+import scala.util.Try
+
 object PokerHands {
 
   val handsPattern = "Black: (.*?)  White: (.*?)".r
@@ -11,15 +13,98 @@ object PokerHands {
         (Hand(cardsBlackPlayer), Hand(cardsWhitePlayer))
     }
 
-    val winningPlayer: Player =
-      if (blackHand.cards.exists(_.value == ACE)) Black else White
+    val (blackHigherScore, blackWinningValue) = blackHand.higherScore
+    val (whiteHigherScore, whiteWinningValue) = whiteHand.higherScore
 
-    GameOutcome(winningPlayer, "High Card: Ace")
+    if (blackHigherScore > whiteHigherScore || (blackHigherScore == whiteHigherScore && blackWinningValue > whiteWinningValue)) {
+      GameOutcome(Black, s"${blackHigherScore.name}: ${blackWinningValue.name}")
+    } else {
+      GameOutcome(White, s"${whiteHigherScore.name}: ${whiteWinningValue.name}")
+    }
   }
 
 }
 
-case class Hand(cards: Seq[Card])
+trait Score extends Ordered[Score] {
+
+  def matches(hand: Hand): Option[Value]
+
+  def name: String
+
+  override def compare(that: Score): Int =
+    Scores.all.indexOf(this) compare Scores.all.indexOf(that)
+
+  protected def maximumValueWithAtLeastOccurrences(cards: Seq[Card], numberOfMinimumOccurrences: Int): Option[Value] = {
+    Try(cards
+      .groupBy(_.value)
+      .mapValues(_.size)
+      .filter { case (_, occurrences) => occurrences >= numberOfMinimumOccurrences}
+      .keys
+      .max
+    ).toOption
+  }
+}
+
+object Scores {
+  lazy val all = Seq(HIGHER_CARD, PAIR, TWO_OF_A_KIND, THREE_OF_A_KIND)
+}
+
+case object HIGHER_CARD extends Score {
+
+  override def matches(hand: Hand) = Some(hand.higherCard.value)
+
+  override def name = "High Card"
+}
+
+case object PAIR extends Score {
+
+  override def matches(hand: Hand) = maximumValueWithAtLeastOccurrences(hand.cards, 2)
+
+  override def name = "Pair"
+}
+
+case object TWO_OF_A_KIND extends Score {
+
+  override def matches(hand: Hand): Option[Value] = for {
+      maximumValuePresentInFirstPair <- maximumValueWithAtLeastOccurrences(hand.cards, 2)
+      maximumValuePresentInSecondPair <- maximumValueWithAtLeastOccurrences(hand.cards.filterNot(card => card.value == maximumValuePresentInFirstPair), 2)
+    } yield maximumValuePresentInFirstPair
+
+  override def name: String = "Two of a Kind"
+}
+
+case object THREE_OF_A_KIND extends Score {
+
+  override def matches(hand: Hand) = maximumValueWithAtLeastOccurrences(hand.cards, 3)
+
+  override def name: String = "Three of a Kind"
+}
+
+trait Player {
+  def name: String
+}
+
+case object Black extends Player {
+  val name = "Black"
+}
+
+case object White extends Player {
+  val name = "White"
+}
+
+case class Hand(cards: Seq[Card]) {
+
+  def higherCard: Card = cards.max
+
+  def higherScore: (Score, Value) = {
+    Scores.all
+      .reverse
+      .map(score => (score, score.matches(this)))
+      .filter { case (score, maybeMatched) => maybeMatched.isDefined }
+      .map { case (score, matched) => (score, matched.get)}
+      .head
+  }
+}
 
 object Hand {
 
@@ -30,17 +115,21 @@ object Hand {
   }
 }
 
-case class Card(value: Value, suit: Suit)
+case class Card(value: Value, suit: Suit) extends Ordered[Card] {
+  override def compare(that: Card): Int = value compare that.value
+}
 
 object Card {
-
   def apply(card: String): Card = {
     require(card.length == 2)
     Card(Value.from(card(0)), Suit.from(card(1)))
   }
 }
 
-abstract class Value
+abstract class Value(val name: String) extends Ordered[Value] {
+  override def compare(that: Value): Int =
+    Value.order.indexOf(this) compare Value.order.indexOf(that)
+}
 
 object Value {
 
@@ -58,22 +147,24 @@ object Value {
     case 'K' => KING
     case 'A' => ACE
   }
+
+  lazy val order = Seq(TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, JACK, QUEEN, KING, ACE)
 }
 
-case object TWO   extends Value
-case object THREE extends Value
-case object FOUR  extends Value
-case object FIVE  extends Value
-case object SIX   extends Value
-case object SEVEN extends Value
-case object EIGHT extends Value
-case object NINE  extends Value
-case object JACK  extends Value
-case object QUEEN extends Value
-case object KING  extends Value
-case object ACE   extends Value
+case object TWO extends Value("Two")
+case object THREE extends Value("Three")
+case object FOUR extends Value("Four")
+case object FIVE extends Value("Five")
+case object SIX extends Value("Six")
+case object SEVEN extends Value("Seven")
+case object EIGHT extends Value("Eight")
+case object NINE extends Value("Nine")
+case object JACK extends Value("Jack")
+case object QUEEN extends Value("Queen")
+case object KING extends Value("King")
+case object ACE extends Value("Ace")
 
-abstract class Suit
+abstract class Suit(val name: String)
 
 object Suit {
 
@@ -85,17 +176,13 @@ object Suit {
   }
 }
 
-case object HEARTS extends Suit
-case object DIAMONDS extends Suit
-case object CLUBS extends Suit
-case object SPADES extends Suit
+case object HEARTS extends Suit("Hearts")
 
+case object DIAMONDS extends Suit("Diamonds")
+
+case object CLUBS extends Suit("Clubs")
+
+case object SPADES extends Suit("Spades")
 
 case class GameOutcome(winner: Player, hand: String)
 
-trait Player {
-  def name: String
-}
-
-case object Black extends Player { val name = "Black" }
-case object White extends Player { val name = "White" }
--- src/test/scala/net/manub/pokerhands/PokerHandsSpec.scala
+++ src/test/scala/net/manub/pokerhands/PokerHandsSpec.scala
@@ -25,6 +25,46 @@ class PokerHandsSpec extends WordSpec with Matchers {
       outcome.winner should be (Black)
       outcome.hand should be ("High Card: Ace")
     }
+
+    "recognise a winner with High Card" in {
+
+      val hands = "Black: 2H 3D 5S 9C JD  White: 2C 3H 4S 8C KH"
+
+      val outcome = PokerHands.outcome(hands)
+
+      outcome.winner should be (White)
+      outcome.hand should be ("High Card: King")
+    }
+
+    "recognise a winner with a pair" in {
+
+      val hands = "Black: 2H 3D 5S JC JD  White: 2C 3H 4S 8C KH"
+
+      val outcome = PokerHands.outcome(hands)
+
+      outcome.winner should be (Black)
+      outcome.hand should be ("Pair: Jack")
+    }
+
+    "recognise a winner with a two of a kind" in {
+
+      val hands = "Black: 2H 3D 5S JC JD  White: 2C 4H 4S KC KH"
+
+      val outcome = PokerHands.outcome(hands)
+
+      outcome.winner should be (White)
+      outcome.hand should be ("Two of a Kind: King")
+    }
+
+    "recognise a winner with a three of a kind" in {
+
+      val hands = "Black: 2H 3D JS JC JD  White: 2C 4H 4S KC KH"
+
+      val outcome = PokerHands.outcome(hands)
+
+      outcome.winner should be (Black)
+      outcome.hand should be ("Three of a Kind: Jack")
+    }
   }
 
 }
